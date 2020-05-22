e0313680b0ac65cbfe70aedf02eaba54753d0838
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-05-22 13:45:27 +0200

    Pulumi.Azure.Extensions: 0.3.0

--- publish.cmd
+++ publish.cmd
@@ -3,6 +3,14 @@ SET tmp_folder=docs-temp
 SET www_folder=%tmp_folder%/wwwroot
 SET docs_folder=docs
 
+dotnet clean src/BlazorAppHandlebarsExample -c Release
 dotnet publish src/BlazorAppHandlebarsExample -c Release -o %tmp_folder%
 
+rem delete all files except .gitattributes
+for /R "%docs_folder%" %%f in (*) do (if not "%%~xf"==".gitattributes" del "%%~f")
+
+rem delete all sub-folders
+for /d %%x in (%docs_folder%\*) do @rd /s /q "%%x"
+
+rem copy all from temp-docs into docs folder
 robocopy %www_folder% %docs_folder% /E
